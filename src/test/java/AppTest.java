11c4f6ae191948c794d8fa52d335c5707dc09e95
Austin Coburn <austincoburn41@gmail.com>
2020-10-14 10:14:23 -0500

    Added a null test scenario

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -14,9 +14,32 @@ class AppTest {
     App app = new App();
 
     @Test
-    void shouldFlattenArray() {
+    void shouldFlattenIntegerArray() {
         List<?> result = app.flattenList(resultList);
         List<Integer> expected = Arrays.asList(2,4,5,8,2,7);
         assertEquals(expected, result);
     }
+
+    @Test
+    void shouldFlattenStringArray() {
+        List<String> stringList1 = Arrays.asList("Hello","My", null, "name", "is");
+        List<String> stringList2 = Arrays.asList(null, "Alec", null, "and", "my name", null, "is", "Austin");
+        List<List<?>> resultList = Arrays.asList(stringList1, stringList2);
+
+        List<?> result = app.flattenList(resultList);
+        List<String> expected = Arrays.asList("Hello","My", "name", "is", "Alec","and","my name","is","Austin");
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void nullListShouldHaveSizeOfZero() {
+        List<String> nullList1 = Arrays.asList(null, null, null);
+        List<String> nullList2 = Arrays.asList(null, null, null, null);
+        List<List<?>> resultList = Arrays.asList(nullList1, nullList2);
+        List<?> result = app.flattenList(resultList);
+        assertEquals(0, result.size());
+    }
+
+
+
 }
