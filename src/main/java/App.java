8fcd979bf75738974a1df254f943a9db8d6b1fca
Wedell3 <wedel024@umn.edu>
2020-10-14 09:49:30 -0500

    created flatten method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,15 @@
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class App {
     public static void main(String[] args) {
+    
+    }
 
+    private static List<?> flattenList(List<List<?>> list) {
+        List<?> result = list.stream().flatMap(Collection::stream).filter(Objects::nonNull).collect(Collectors.toList());
+        return result;
     }
 }
